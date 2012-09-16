7b97af9f7334f07fecad2d89dcad6c6359acd02e
Amir Friedman <amir@crossrider.com>
2012-09-16 23:18:01 +0300

    adding some more specs to the flush method

--- spec/cotendo_spec.rb
+++ spec/cotendo_spec.rb
@@ -12,5 +12,13 @@ describe Cotendo do
     it "can do it" do
       client.flush(config[:cname], '/foo')
     end
+
+    it "method exists" do
+      client.should respond_to (:flush)
+    end
+
+    it "get back the expected response" do
+      client.flush(config[:cname], '/foo').should eq({:do_flush_response=>{:"@xmlns:ns2"=>"http://api.cotendo.net/"}})
+    end
   end
 end
